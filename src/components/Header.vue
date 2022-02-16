fce249c98922f14a3af58fb08d9e32ee2f5e029b
judith lourde <judith.lourde@gmail.com>
2022-02-16 23:05:03 +0100

    Add Header Component

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <header class="header">
+        <div class="header__logo">
+            <img src="/images/svg/logo.svg" alt="money-logo">
+            <h1>ALL CRYPTOCURRENCIES PRICES</h1>
+        </div>
+
+        <div class="header__hamburger">
+            <img src="/images/svg/hamburger.svg" alt="hamburger-icon">
+        </div>  
+    </header>
+</template>
+
+<style>
+    .header {
+        margin: 20px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 150px;
+    }
+
+    .header__logo {
+        margin-left: 100px;
+        display: flex;
+        align-items: center;
+
+    }
+
+    .header__logo img {
+        width: 70px;
+        margin-right: 30px;
+    }
+
+    .header__hamburger img {
+        width: 30px;
+        margin-right: 140px;
+    }
+</style>
